5471b572b4136ccca31f0ced3f3c2476cca394ea
andrezacm <andreza@csu.fullerton.edu>
2016-05-05 00:53:08 -0300

    Add insertion

--- abb.rb
+++ abb.rb
@@ -11,6 +11,10 @@ class Tree
     @root.include?(key)
   end
 
+  def add_node(key)
+    @root.add_node(Node.new(key))
+  end
+
   class Node
 
     attr_accessor :left, :right, :parent, :key, :value, :how_many_nodes_left, :how_many_nodes_right
@@ -33,9 +37,46 @@ class Tree
       return false 
     end
 
+    def add_node(node)
+      case key <=> node.key
+      when 1  then add_left(node)
+      when -1 then add_right(node)
+      when 0  then true
+      end      
+    end
+
     def inspect
       "{#{key}::#{left.inspect}|#{right.inspect}}"
     end
+
+
+    private 
+
+      def add_left(node)
+        if how_many_nodes_left == 0 then
+          add_node_in_left_tree(node)
+        else
+          left.add_node(node)
+        end
+      end
+      
+      def add_right(node)
+        if how_many_nodes_right == 0 then
+          add_node_in_right_tree(node)
+        else
+          right.add_node(node)
+        end
+      end
+
+      def add_node_in_left_tree(node)
+        @left = node
+        @how_many_nodes_left+=1
+      end
+
+      def add_node_in_right_tree(node)
+        @right = node
+        @how_many_nodes_right+=1
+      end
 
   end
 
